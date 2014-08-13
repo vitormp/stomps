49ddd38b20522977450c4f8bbd7459e28635994c
Vitor Monteiro Puente <vitor.m.p22@gmail.com>
2014-08-13 11:17:31 -0300

    Creating the connect method. Now it is possible to connect to a message broker with a STOMPClient specifying the server host and the server port

--- src/main/scala/com/stomp/STOMPClient.scala
+++ src/main/scala/com/stomp/STOMPClient.scala
@@ -3,12 +3,18 @@ package com.stomp
 import java.io.PrintWriter
 import java.net.Socket
 
-import com.stomp.message.STOMPMessage
+import com.stomp.message.{Connect, STOMPMessage}
 
 /**
  * Created by vmp on 8/8/14.
  */
-class STOMPClient(serverHost : String, serverPort : Integer) {
+class STOMPClient(serverHost : String = "127.0.0.1", serverPort : Integer = 61613) {
+
+  def connect() = {
+    val server = new Socket(serverHost, serverPort)
+    val client = new PrintWriter(server.getOutputStream())
+    client.println(Connect)
+  }
 
   def send(message : STOMPMessage) = {
     val server = new Socket(serverHost, serverPort)
--- src/test/scala/com/stomp/STOMPClientTest.scala
+++ src/test/scala/com/stomp/STOMPClientTest.scala
@@ -8,8 +8,16 @@ import com.stomp.test.core.UnitSpec
  */
 class STOMPClientTest extends UnitSpec {
 
-  "A Client" should "connect to a message broker" in {
-    val client : STOMPClient = new STOMPClient("127.0.0.1", 61613)
+  "A Client" should "connect to a message broker sending a STOMP message of CONNECT type" in {
+    val client: STOMPClient = new STOMPClient("127.0.0.1", 61613)
     client.send(Connect(""))
+  }
+
+  "A Client" should "connect to message broker when calling the connect method without parameters" in {
+    new STOMPClient().connect();
+  }
+
+  "A Client" should "connect to message broker when calling the connect method specifying the host" in {
+    new STOMPClient("127.0.0.1").connect();
   }
 }
